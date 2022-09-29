fecc3fabda6adccc1d14c56cf5b670ececa9ca7c
Henok Hailemariam <henokhm2@gmail.com>
2022-09-29 21:47:24 +0300

    Update image versions

--- udagram-api-user/src/sequelize.ts
+++ udagram-api-user/src/sequelize.ts
@@ -1,6 +1,17 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
+console.log(
+  {
+  'username': config.username,
+  'password': config.password,
+  'database': config.database,
+  'host': config.host,
+
+  'dialect': config.dialect,
+  'storage': ':memory:',
+}
+)
 
 export const sequelize = new Sequelize({
   'username': config.username,
